b0ca669099b1647bc2d753c15bd9bac122f22e06
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-11-21 05:03:24 -0500

    add argument parsing

--- client.py
+++ client.py
@@ -6,6 +6,13 @@ from imutils.video import VideoStream
 import imagezmq
 import cv2
 
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--device", help="window, guest, or windowBack")
+args = parser.parse_args()
+
 sender = imagezmq.ImageSender(connect_to='tcp://localhost:5555')
 
 rpi_name = socket.gethostname() # send RPi hostname with each image
@@ -14,7 +21,7 @@ picam = VideoStream().start()
 time.sleep(2.0)  # allow camera sensor to warm up
 while True:  # send images as stream until Ctrl-C
     image = picam.read()
-    new_img_str = sender.send_image("window", image)
+    new_img_str = sender.send_image(args.device, image)
 
     nparr = np.fromstring(new_img_str, np.uint8)
     rec_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
