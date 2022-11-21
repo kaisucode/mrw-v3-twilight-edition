fdfb6cbc91f29305fb0991742f495636eea29290
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-11-21 03:46:28 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+try using worker threads for mediapipe processing
+
+look [here](https://docs.python.org/3/library/queue.html#queue.Queue.join) for examples
+
---
+++ server.py
@@ -0,0 +1,59 @@
+import cv2
+import imagezmq
+import mediapipe as mp
+import queue
+
+def replace_background(fg, bg):
+    bg_image = bg
+    frame = fg
+
+    RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # get the result
+    results = selfie_segmentation.process(RGB)
+
+    mask = results.segmentation_mask
+    mask = cv2.GaussianBlur(mask, (33, 33), 0)
+
+    # it returns true or false where the condition applies in the mask
+    condition = np.stack(
+        (mask,) * 3, axis=-1) > 0.6
+    height, width = frame.shape[:2]
+    # resize the background image to the same size of the original frame
+    bg_image = cv2.resize(bg_image, (width, height))
+    output_image = np.where(condition, frame, bg_image)
+    return output_image
+
+
+if __name__ == "__main__":
+
+    image_hub = imagezmq.ImageHub()
+
+    print("Starting server on port 5555")
+
+    #  The queue module implements multi-producer, multi-consumer queues. 
+    #  It is especially useful in threaded programming when information must be exchanged safely between multiple threads. 
+    #  The Queue class in this module implements all the required locking semantics.
+
+    guestQueue = queue.Queue(maxsize=3)
+    windowQueue = queue.Queue(maxsize=3)
+    windowBackQueue = queue.Queue(maxsize=3)
+
+    mp_selfie_segmentation = mp.solutions.selfie_segmentation
+    selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation()
+
+
+    frame = 0
+    while True: 
+        rpi_name, image = image_hub.recv_image()
+
+        cv2.imshow(rpi_name, image)
+        cv2.waitKey(1)
+        image_hub.send_reply(b'OK')
+        
+        if (frame % 10 == 0): 
+            print("received image from ", rpi_name)
+
+        frame += 1
+
+
+
